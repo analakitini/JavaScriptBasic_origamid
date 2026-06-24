9c26743ffe336a0b617c9d307d88e2a27a467234
Ana Lakitini <carollakitini@hotmail.com>
2026-06-24 11:54:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+console.log(nav);
+console.log(produtos);
+
+console.log(produtos.href);
+
+console.dir(nav);
+
+nav.style.backgroundColor = "blue";
+nav.style.padding = "10px";
+
+nav.classList.add("menu");
