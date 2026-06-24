c0069ceac43b76af5b138ff738153563b5d4ab35
Ana Lakitini <carollakitini@hotmail.com>
2026-06-24 12:16:10 -0300

    Manipulating the DOM

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/*Manipulating the DOM*/
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+console.log(nav);
+console.log(produtos);
+
+console.log(produtos.href);
+console.dir(nav);
+nav.style.backgroundColor = "blue";
+nav.style.padding = "10px";
+nav.classList.add("menu");
+
 /*Strings*/
 
 const text = "JavaScript";
