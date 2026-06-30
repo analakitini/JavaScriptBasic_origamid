a3fc221eb30b2336f41513cace4c674d9d2b21e4
Ana Lakitini <carollakitini@hotmail.com>
2026-06-30 10:29:54 -0300

    arrays e loops

---
+++ arrays_loops/script.js
@@ -0,0 +1,25 @@
+/*arrays são listas de dados, que podem ser de qualquer tipo, como números, strings, booleanos, objetos e até mesmo outras listas.*/
+
+/* const lista = ["JavaScript", 20, true];
+
+const nome = lista[0];
+console.log(nome); */
+
+/* loops são estruturas de repetição, que permitem executar um bloco de código várias vezes, enquanto uma condição for verdadeira.*/
+/* i++ é um operador de incremento, que aumenta o valor de uma variável em 1.*/
+
+/* const lista = ["JavaScript", "hTML", "CSS"];
+
+for (let i = 0; i < 100; i++) {
+  console.log("teste" + i);
+} */
+
+/* usando o length para percorrer a lista */
+
+const lista = ["JavaScript", "hTML", "CSS", "PHP"];
+
+const body = document.querySelector("body");
+
+for (let i = 0; i < lista.length; i++) {
+  body.innerHTML += "<li>" + lista[i] + "</li>";
+}
